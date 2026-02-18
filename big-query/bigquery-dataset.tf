aaddf1ee2e79f5559e1e53180d8a9685f5dea7df
ateneva <tenevaa21@gmail.com>
2026-02-18 22:51:45 +0100

    CU-86c83gjj7-define-eu-data-challenge-dataset

--- big-query/bigquery-dataset.tf
+++ big-query/bigquery-dataset.tf
@@ -1,10 +1,10 @@
 
-resource "google_bigquery_dataset" "the-data-challenge" {
-  dataset_id                  = "the_data_challenge"
-  friendly_name               = "the-data-challenge"
+resource "google_bigquery_dataset" "eu-data-challenge" {
+  dataset_id                  = "eu_data_challenge"
+  friendly_name               = "eu-data-challenge"
   description                 = "This dataset stores the data-engineering challenge"
   location                    = "EU"
-  default_table_expiration_ms = 3600000
+  default_table_expiration_ms = 1296000000  # 15 days
   labels = {
     env = "default"
   }
